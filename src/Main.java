4379f4b31a4360c3b46bd5a80e817df42f8f8d22
Michal Swierczynski <hajzenberk@gmail.com>
2025-01-29 18:13:19 +0100

    Added comments and user choice

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,30 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+
         Random random = new Random();
 
+        Scanner scanner = new Scanner(System.in);
 
+        //Random boolean for value true or false
         boolean isHeads = random.nextBoolean();
 
+        System.out.println("\nChoose heads or tails");
+        String userChoice = scanner.nextLine();
+        System.out.println("Your choice is " + userChoice);
+
+        //if statement to print the message for the user
         if (isHeads) {
             System.out.println("\nHeads");
         } else {
             System.out.println("\nTails");
         }
+
+        scanner.close();
 
     }
 
